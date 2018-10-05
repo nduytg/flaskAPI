822496fbf5069636b91a74bf9aaea9814619ca71
Duy Nguyen <nduytg@gmail.com>
2018-10-05 10:17:54 +0700

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 container="flaskAPI"
-ip="172.19.0.6"
 
 docker rm -f $container
-docker run --name $container --hostname $container --network=brlgr --ip=$ip -v /data/rsyncAPI/app:/app:rw -v /home/lgr/CDN:/app/cdn:ro --restart always -d mini-api
+docker run --name $container --hostname $container -p 80:80 --restart always -d mini-api
